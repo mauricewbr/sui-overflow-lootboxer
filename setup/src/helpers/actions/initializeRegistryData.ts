dc5d54856f2c7158a42b3b6f5a6a857a0e9216c8
mauricewbr <39859692+mauricewbr@users.noreply.github.com>
2024-05-31 14:54:24 +0200

    added gas payment for tx

--- setup/src/helpers/actions/initializeRegistryData.ts
+++ setup/src/helpers/actions/initializeRegistryData.ts
@@ -1,5 +1,4 @@
 import { SuiClient, SuiObjectChangeCreated } from '@mysten/sui.js/client';
-import { Ed25519Keypair } from '@mysten/sui.js/keypairs/ed25519';
 import { TransactionBlock } from '@mysten/sui.js/transactions';
 import { getBLSPublicKey } from '../bls/getBLSPublicKey';
 import { getKeypair } from '../keypair/getKeyPair';
@@ -24,21 +23,26 @@ export const initializeRegistryData = async ({
     let adminBLSPublicKey = getBLSPublicKey(ADMIN!);
     console.log('adminBLSPubKey: ', adminBLSPublicKey);
 
+    tx.setGasPayment([
+        {
+            digest: '3MMDHBMEiFXfLmrB9gZLkk8pZJnnxcouv5eeCuQSbZm2',
+            objectId:
+                '0x2ed93acb09c94e464295e7842869dfba08842d4f906131c3b7361a730469a1e4',
+            version: 976670,
+        },
+    ]); // TODO: Change hardcoded values
+    tx.setGasBudget(1000000000);
     tx.moveCall({
         target: `${PACKAGE_ADDRESS}::registry::initialize_registry_data`,
-        arguments: [
-            tx.object(REGISTRY_CAP),
-            // tx.pure(Array.from(adminBLSPublicKey)),
-        ],
+        arguments: [tx.object(REGISTRY_CAP)],
     });
 
-    const keypair = new Ed25519Keypair();
-    console.log('keypair: ', keypair);
+    const keypair = getKeypair(ADMIN!);
+    console.log(keypair);
 
     return suiClient
         .signAndExecuteTransactionBlock({
-            signer: getKeypair(ADMIN!),
-            // signer: keypair,
+            signer: keypair,
             transactionBlock: tx,
             requestType: 'WaitForLocalExecution',
             options: {
